30047905302dd114ef9525bd30381d8fcdb24ca5
Ashley Huffman <ashuffman9452@gmail.com>
2025-12-05 13:38:37 -0600

    Fixed a bug where redrawing affinities would not work properly, also fixed a bug where if you insert your input without space, an IndexError crash would occur outside of try/except

--- src/project.py
+++ src/project.py
@@ -54,67 +54,71 @@ def score_calculator(selected_hand, returned_library):
 
 def inventory(returned_library):
     hand = []
-    hand_counter = 0
     affinity_list, spell_list = returned_library
     affinity_inventory = affinity_list["affinity_list"] * 7
-    inventory_data = generate_hand(hand, hand_counter, affinity_inventory), affinity_inventory
+    inventory_data = generate_hand(hand, affinity_inventory), affinity_inventory
     return inventory_data
 
-def generate_hand(hand, hand_counter, affinity_inventory):
-    while hand_counter < 7:
-        hand_counter += 1
+def generate_hand(hand, affinity_inventory):
+    while len(hand) < 7:
         idx = random.randrange(len(affinity_inventory))
         hand.append(f"{affinity_inventory[idx]}")
         affinity_inventory.pop(idx)
-        generated_hand = hand, hand_counter
+        generated_hand = hand
     return generated_hand
 
 
 def play_hand():
     returned_library = spell_library()
     generated_player_hand_data, affinity_inventory = inventory(returned_library)
+    player_hand = generated_player_hand_data
     selected_hand = []
     redraw_counter = 3
     selection_process = True
     # TODO: Add a way to discard and redraw affinities
     while selection_process == True:
-        affinity_selection = True
         hand_confirmation = True
-        while affinity_selection == True:    
-            try:
-                player_hand, hand_counter = generated_player_hand_data
-                print(player_hand)
-                if redraw_counter > 0:
-                    selection = input(f"Select up to 5 affinities by typing their index(1-7), or select up to {redraw_counter} starting with 'r' to redraw, seperated by spaces: ")
-                else:
-                    selection = input(f"You have no more redraws, select up to 5 affinities by typing their index(1-7) seperated by spaces: ")
-                inputs = selection.split()
-                if len(inputs) > 5:
-                    print("You can only select up to 5 affinities!")
-                elif inputs[0] == "r":
-                    inputs.pop(0)
-                    if len(inputs) > redraw_counter:
-                        print(f"You cannot redraw more than {redraw_counter} affinities!")
+        check_index_error = True
+        while check_index_error == True:
+            affinity_selection = True
+            while affinity_selection == True:    
+                try:
+                    print(player_hand)
+                    if redraw_counter > 0:
+                        selection = input(f"Select up to 5 affinities by typing their index(1-7), or select up to {redraw_counter} starting with 'r' to redraw, seperated by spaces: ")
+                    else:
+                        selection = input(f"You have no more redraws, select up to 5 affinities by typing their index(1-7) seperated by spaces: ")
+                    inputs = selection.split()
+                    if len(inputs) > 5:
+                        print("You can only select up to 5 affinities!")
+                    elif inputs[0] == "r":
+                        inputs.pop(0)
+                        if len(inputs) > redraw_counter:
+                            print(f"You cannot redraw more than {redraw_counter} affinities!")
+                        else:
+                            integer_selection = [int(item) for item in inputs]
+                            sorted_integer_selection = sorted(integer_selection, reverse=True)
+                            print(sorted_integer_selection)
+                            for number in sorted_integer_selection:
+                                int_number = number - 1
+                                redraw_counter -= 1
+                                player_hand.pop(int_number)
+                            generate_hand(player_hand, affinity_inventory)
                     else:
                         integer_selection = [int(item) for item in inputs]
-                        for number in integer_selection:
-                            int_number = number - 1
-                            player_hand.pop(int_number)
-                            hand_counter -= 1
-                            redraw_counter -= 1
-                        generate_hand(player_hand, hand_counter, affinity_inventory)
-                else:
-                    integer_selection = [int(item) for item in inputs]
-                    affinity_selection = False
-            except ValueError:
-                print("Invalid input, try again.")
+                        affinity_selection = False
+                except ValueError:
+                    print("Invalid input, try again.")
+                except IndexError:
+                    print("Invalid input, try again.")
+            try:
+                for number in integer_selection:
+                    int_number = number - 1
+                    selected_affinity = player_hand[int_number]
+                    selected_hand.append(selected_affinity)
+                check_index_error = False
             except IndexError:
                 print("Invalid input, try again.")
-
-        for number in integer_selection:
-            int_number = number - 1
-            selected_affinity = player_hand[int_number]
-            selected_hand.append(selected_affinity)
                
         while hand_confirmation == True:
             confirmation = input(f"Play {selected_hand}?(Y/N): ")
